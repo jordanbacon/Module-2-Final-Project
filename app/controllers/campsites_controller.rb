fa47c763aa0a18d4226c3f2e3935dfd4ade97f8c
jordanbacon <jmbacon92@gmail.com>
2019-07-29 12:56:47 -0700

    working on campsite controller

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,5 +1,45 @@
 class CampsitesController < ApplicationController
 
+    def index
+        @campsites = Campsite.all
+        @user = User.find(session[:current_user_id])
+    end
 
+    def show
+        @campsite = Campsite.find(params[:id])
+        @user = User.find(session[:current_user_id])
+    end
+
+    def new
+        @campsite = Campsite.all
+        @booking = Booking.new
+    end
+
+    def create
+        @campsite = Campsite.create(campsite_params)
+        redirect_to campsite_path(@campsite)
+    end
+
+    def edit
+        @campsite = Campsite.find(params[:id])
+        @user = User.find(session[:cureent_user_id])
+        @bookings = @user.bookings
+    end
+
+    def update
+        @campsite = Campsite.find(params[:id])
+        @campsite.update(campsite_params)
+        redirect_to campsite_path(@campsite)
+    end
+
+    def delete
+    end
+      
+        private
+      
+        def campsite_params
+          params.require(:campsite).permit(:site_number, :location, :accomodation, :length_of_stay, :price)
+        end
+      end
 
 end
