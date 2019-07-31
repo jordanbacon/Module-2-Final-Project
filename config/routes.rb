48075adc155b412922a5f81bf39b74964a23a7ae
jordanbacon <jmbacon92@gmail.com>
2019-07-31 15:50:15 -0700

    added slot updates to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,13 @@ Rails.application.routes.draw do
   resources :bookings
   resources :campsites
 
+  post '/edit_slot', to: 'slots#update'
+  resources :slots, only: [:show]
+
   resources :sessions, only: [:new, :create, :destroy, :index]
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
